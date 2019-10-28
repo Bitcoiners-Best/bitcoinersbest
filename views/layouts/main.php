624dbfe62faabbd5c1ac67b373133e1b4b811ff4
andy <gfxdsign@gmail.com>
2019-10-27 20:19:40 -0700

    remove email signup + add twitter button

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -97,6 +97,7 @@ AppAsset::register($this);
           <div class="flex-grow-1 flex-row">
             <h6 class="c-white">&copy; Bitcoiner's Best <?= date('Y') ?><a class="link ml-20" href="">Terms</a><a class="link ml-20" href="">Privacy</a></h6>
           </div>
+          <h6 class="c-white m-0"><a class="link" href="bitcoinersbest@pm.me">Contact</a></h6>
           <h6 class="c-white m-0"><a class="link" href="">@bitcoinersbest</a></h6>
       </div>
     </div>
