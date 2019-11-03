b96d90a5de626bf74bff9622c40a70615352a1c1
andy <gfxdsign@gmail.com>
2019-11-02 20:01:27 -0800

    class tweaks

--- views/admin/res-item/_form.php
+++ views/admin/res-item/_form.php
@@ -12,7 +12,7 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'res_type_id')->dropDownList(\app\models\ResType::getAvailableTypesAsArray(),['style'=>'width:150px'])->label('Content Type') ?>
+    <?= $form->field($model, 'res_type_id')->dropDownList(\app\models\ResType::getAvailableTypesAsArray(),['style'=>'width:200px'])->label('Content Type') ?>
 
     <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
 
