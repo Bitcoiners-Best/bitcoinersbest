ccca4c7dfa2bdecd96ea1d2e54d686a6d842266e
andy <gfxdsign@gmail.com>
2019-11-03 18:39:00 -0800

    footer partial

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -107,17 +107,7 @@ AppAsset::register($this);
     </div>
 </div>
 
-<footer class="footer bg-black">
-    <div class="container-fluid">
-      <div class="d-flex pb-30 pt-30">
-          <div class="flex-grow-1 flex-row">
-            <h6 class="c-white">&copy; Bitcoiner's Best <?= date('Y') ?><a class="link ml-20" href="">Terms</a><a class="link ml-20" href="">Privacy</a></h6>
-          </div>
-          <h6 class="c-white m-0"><a class="link" href="bitcoinersbest@pm.me">Contact</a></h6>
-          <h6 class="c-white m-0"><a class="link" target="_blank" href="https://twitter.com/bitcoiners_best">@bitcoiners_best</a></h6>
-      </div>
-    </div>
-</footer>
+<?php echo $this->render('_footer'); ?>
 
 <?php $this->endBody() ?>
 </body>
