60a01bad634a20c88833625f4827db8a5558a302
andy <gfxdsign@gmail.com>
2019-11-05 00:12:44 -0800

    dropdown visibility toggle

--- views/admin/res-item/_form.php
+++ views/admin/res-item/_form.php
@@ -12,22 +12,71 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'res_type_id')->dropDownList(\app\models\ResType::getAvailableTypesAsArray(),['style'=>'width:200px'])->label('Resource Type') ?>
+    <?= $form->field($model, 'res_type_id')->dropDownList(\app\models\ResType::getAvailableTypesAsArray())->label('Resource Type') ?>
 
-    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+    <div class="resource-type 20">
+      <h2 class="mb-20">Podcast</h2>
+      <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+      <?= $form->field($model, 'description')->textInput(['maxlength' => true]) ?>
+      <?= $form->field($model, 'image')->textInput(['maxlength' => true]) ?>
+      <?= $form->field($model, 'rss')->textInput(['maxlength' => true]) ?>
+      <?= $form->field($model, 'url')->textInput(['maxlength' => true]) ?>
+      <?= $form->field($model, 'created_by')->textInput(['maxlength' => true]) ?>
+      <div class="form-group mt-40">
+          <?= Html::submitButton('Submit', ['class' => 'btn btn-rect-xl']) ?>
+      </div>
+    </div>
 
-    <?= $form->field($model, 'description')->textInput(['maxlength' => true]) ?>
+    <div class="resource-type 25">
+      <h2 class="mb-20">Episode</h2>
+      <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+      <?= $form->field($model, 'description')->textInput(['maxlength' => true]) ?>
+      <?= $form->field($model, 'image')->textInput(['maxlength' => true]) ?>
+      <?= $form->field($model, 'rss')->textInput(['maxlength' => true]) ?>
+      <?= $form->field($model, 'url')->textInput(['maxlength' => true]) ?>
+      <?= $form->field($model, 'created_by')->textInput(['maxlength' => true]) ?>
+      <div class="form-group mt-40">
+          <?= Html::submitButton('Submit', ['class' => 'btn btn-rect-xl']) ?>
+      </div>
+    </div>
 
-    <?= $form->field($model, 'image')->textInput(['maxlength' => true]) ?>
+    <div class="resource-type 30">
+      <h2 class="mb-20">Article</h2>
+      <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+      <?= $form->field($model, 'description')->textInput(['maxlength' => true]) ?>
+      <?= $form->field($model, 'image')->textInput(['maxlength' => true]) ?>
+      <?= $form->field($model, 'rss')->textInput(['maxlength' => true]) ?>
+      <?= $form->field($model, 'url')->textInput(['maxlength' => true]) ?>
+      <?= $form->field($model, 'created_by')->textInput(['maxlength' => true]) ?>
+      <div class="form-group mt-40">
+          <?= Html::submitButton('Submit', ['class' => 'btn btn-rect-xl']) ?>
+      </div>
+    </div>
 
-    <?= $form->field($model, 'rss')->textInput(['maxlength' => true]) ?>
+    <div class="resource-type 35">
+      <h2 class="mb-20">Book</h2>
+      <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+      <?= $form->field($model, 'description')->textInput(['maxlength' => true]) ?>
+      <?= $form->field($model, 'image')->textInput(['maxlength' => true]) ?>
+      <?= $form->field($model, 'rss')->textInput(['maxlength' => true]) ?>
+      <?= $form->field($model, 'url')->textInput(['maxlength' => true]) ?>
+      <?= $form->field($model, 'created_by')->textInput(['maxlength' => true]) ?>
+      <div class="form-group mt-40">
+          <?= Html::submitButton('Submit', ['class' => 'btn btn-rect-xl']) ?>
+      </div>
+    </div>
 
-    <?= $form->field($model, 'url')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'created_by')->textInput(['maxlength' => true]) ?>
-
-    <div class="form-group">
-        <?= Html::submitButton('Submit', ['class' => 'btn btn-rect-xl']) ?>
+    <div class="resource-type 40">
+      <h2 class="mb-20">Twitter Thread</h2>
+      <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+      <?= $form->field($model, 'description')->textInput(['maxlength' => true]) ?>
+      <?= $form->field($model, 'image')->textInput(['maxlength' => true]) ?>
+      <?= $form->field($model, 'rss')->textInput(['maxlength' => true]) ?>
+      <?= $form->field($model, 'url')->textInput(['maxlength' => true]) ?>
+      <?= $form->field($model, 'created_by')->textInput(['maxlength' => true]) ?>
+      <div class="form-group mt-40">
+          <?= Html::submitButton('Submit', ['class' => 'btn btn-rect-xl']) ?>
+      </div>
     </div>
 
     <?php ActiveForm::end(); ?>
