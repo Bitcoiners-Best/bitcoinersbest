24316b6ce940bd4ebdf233776e9578b4b40b93f0
andy <gfxdsign@gmail.com>
2019-11-06 01:49:09 -0800

    removes

--- views/site/faq.php
+++ views/site/faq.php
@@ -6,20 +6,18 @@ use yii\helpers\Html;
 $this->title = 'Frequently Asked Questions';
 ?>
 
-<div class="section-header pt-20 pb-20 bg-white">
-  <div class="section-header d-flex">
+<div class="section-header pt-20 pb-20 c-white text-center">
       <h2 class="mb-0"><?= Html::encode($this->title) ?></h2>
-  </div>
 </div>
 
-<h5 class="mt-30">What is Bitcoiners' Best?<h5>
-<p class="c-gray-1">Bitcoiners' Best is a community ... of the best. Discover the best bitcoin content and upvote your favorite ones!</p>
-<p class="c-gray-1">Created by <a class="link" target="_blank" href="https://twitter.com/CitizenBitcoin">@CitizenBitcoin</a>, <a class="link" target="_blank" href="https://twitter.com/BootstrapBandit">@BootstrapBandit</a>, and <a class="link" target="_blank" href="https://twitter.com/binarydreaming">@binarydreaming</a></p>
+<h3 class="mt-30 c-white">What is Bitcoiners' Best?<h3>
+<p class="c-white">Bitcoiners' Best is a community ... of the best. Discover the best bitcoin content and upvote your favorite ones!</p>
+<p class="c-white">Created by <a class="link" target="_blank" href="https://twitter.com/CitizenBitcoin">@CitizenBitcoin</a>, <a class="link" target="_blank" href="https://twitter.com/BootstrapBandit">@BootstrapBandit</a>, and <a class="link" target="_blank" href="https://twitter.com/binarydreaming">@binarydreaming</a></p>
 
-<h5 class="mt-30">What can I submit to Lapps.co?<h5>
-<p class="c-gray-1">The ideal submission is any bitcoin-only related content creattion, a product or platform that exclusively uses or talks about Bitcoin.</p>
-<p class="c-gray-1">However, if you have a book, article, podcast, or platform that you think would be a good fit, please free to submit it!</p>
+<h4 class="mt-30 c-white">What can I submit to Lapps.co?<h4>
+<p class="c-white">The ideal submission is any bitcoin-only related content creattion, a product or platform that exclusively uses or talks about Bitcoin.</p>
+<p class="c-white-offset">However, if you have a book, article, podcast, or platform that you think would be a good fit, please free to submit it!</p>
 
-<h5 class="mt-30">Where does the money go?<h5>
-<p class="c-gray-1">All of the money that is submitted to upvote content is pooled together and donated monthly to a voted on open-source project.</p>
-<p class="c-gray-1">We're supporting the bitcoin community, and that will never change.</p>
+<h5 class="mt-30 c-white">Where does the money go?<h5>
+<p class="c-white">All of the money that is submitted to upvote content is pooled together and donated monthly to a voted on open-source project.</p>
+<p class="c-white">We're supporting the bitcoin community, and that will never change.</p>
