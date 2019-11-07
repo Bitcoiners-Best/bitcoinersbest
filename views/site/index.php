4f1ea42d16fdce1bb1047e803dbaa8e739e85036
andy <gfxdsign@gmail.com>
2019-11-07 13:45:53 -0800

    improvements

--- views/site/index.php
+++ views/site/index.php
@@ -19,7 +19,7 @@ $this->title = 'Home';
   <div class="article-module d-flex br-4 mb-30">
       <div class="flex-grow-1 align-self-center">
           <img src="http://lorempixel.com/400/160" class="card-img" alt="...">
-          <h6 class="text-uppercase c-gray-1 medium mt-20 mb-10">TALES FROM THE CRYPT</h6>
+          <h6 class="text-uppercase c-gray-1 medium mt-25 mb-15">TALES FROM THE CRYPT</h6>
           <h5 class="medium mb-15 c-white">Tales from the Crypt #34: Murad Mahmudov</h5>
           <h5 class="regular c-gray-1">With the price of a bitcoin surging to new highs in 2019, the bullish case for investors might seem so obvious it does not need...</h5>
       </div>
@@ -45,12 +45,81 @@ $this->title = 'Home';
       <div class="align-self-top pl-20">
           <div class="votes text-center">
               <h6 class="medium mb-5 c-white">535</h6>
+              <button class="btn bg-brand br-circle icon-wrap push-button">
+                <span class="icon a a-link plus rounded"></span>
+              </button>
+          </div>
+      </div>
+  </div>
+
+  <!-- <div class="episode-module d-flex br-4 mb-30">
+      <div class="pr-20">
+          <img src="http://lorempixel.com/100/100" class="ew-100 card-img" alt="...">
+      </div>
+      <div class="flex-grow-1 align-self-center">
+          <h6 class="text-uppercase c-gray-1 medium mb-15">TALES FROM THE CRYPT</h6>
+          <h5 class="medium c-white">Tales from the Crypt #34: Murad Mahmudov</h5>
+      </div>
+      <div class="align-self-top pl-20">
+          <div class="votes text-center">
+              <h6 class="medium mb-5 c-white">535</h6>
               <button class="btn bg-brand br-circle icon-wrap">
                 <span class="icon a a-link plus rounded"></span>
               </button>
           </div>
       </div>
   </div>
+  <div class="episode-module d-flex br-4 mb-30">
+      <div class="pr-20">
+          <img src="http://lorempixel.com/100/100" class="ew-100 card-img" alt="...">
+      </div>
+      <div class="flex-grow-1 align-self-center">
+          <h6 class="text-uppercase c-gray-1 medium mb-15">TALES FROM THE CRYPT</h6>
+          <h5 class="medium c-white">Tales from the Crypt #34: Murad Mahmudov</h5>
+      </div>
+      <div class="align-self-top pl-20">
+          <div class="votes text-center">
+              <h6 class="medium mb-5 c-white">535</h6>
+              <button class="btn bg-brand br-circle icon-wrap">
+                <span class="icon a a-link plus rounded"></span>
+              </button>
+          </div>
+      </div>
+  </div>
+  <div class="episode-module d-flex br-4 mb-30">
+      <div class="pr-20">
+          <img src="http://lorempixel.com/100/100" class="ew-100 card-img" alt="...">
+      </div>
+      <div class="flex-grow-1 align-self-center">
+          <h6 class="text-uppercase c-gray-1 medium mb-15">TALES FROM THE CRYPT</h6>
+          <h5 class="medium c-white">Tales from the Crypt #34: Murad Mahmudov</h5>
+      </div>
+      <div class="align-self-top pl-20">
+          <div class="votes text-center">
+              <h6 class="medium mb-5 c-white">535</h6>
+              <button class="btn bg-brand br-circle icon-wrap">
+                <span class="icon a a-link plus rounded"></span>
+              </button>
+          </div>
+      </div>
+  </div>
+  <div class="episode-module d-flex br-4 mb-30">
+      <div class="pr-20">
+          <img src="http://lorempixel.com/100/100" class="ew-100 card-img" alt="...">
+      </div>
+      <div class="flex-grow-1 align-self-center">
+          <h6 class="text-uppercase c-gray-1 medium mb-15">TALES FROM THE CRYPT</h6>
+          <h5 class="medium c-white">Tales from the Crypt #34: Murad Mahmudov</h5>
+      </div>
+      <div class="align-self-top pl-20">
+          <div class="votes text-center">
+              <h6 class="medium mb-5 c-white">535</h6>
+              <button class="btn bg-brand br-circle icon-wrap">
+                <span class="icon a a-link plus rounded"></span>
+              </button>
+          </div>
+      </div>
+  </div> -->
 
 
   <div class="book-module d-flex br-4 mb-30">
