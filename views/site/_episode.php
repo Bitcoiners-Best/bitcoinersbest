53b013d1e4dc0219828bf39cfeb7bc22d382226d
andy <gfxdsign@gmail.com>
2019-11-09 13:33:24 -0800

    basic implementation

---
+++ views/site/_episode.php
@@ -0,0 +1,22 @@
+<?php
+   use yii\helpers\Html;
+   use yii\helpers\HtmlPurifier;
+?>
+
+<div class="episode-module d-flex br-4 mb-30">
+   <div class="pr-20">
+       <img src="<?= $model->image ?>" class="ew-100 card-img" alt="...">
+   </div>
+   <div class="flex-grow-1 align-self-center">
+       <h6 class="text-uppercase c-gray-1 medium mb-15"><?= $model->created_at ?></h6>
+       <h5 class="medium c-white"><?= $model->title ?></h5>
+   </div>
+   <div class="align-self-top pl-20">
+       <div class="votes text-center">
+           <h6 class="medium mb-5 c-white"><?= $model->vote_count ?></h6>
+           <button class="btn bg-brand br-circle icon-wrap push-button">
+             <span class="icon a a-link plus rounded"></span>
+           </button>
+       </div>
+   </div>
+</div>
---
+++ views/site/_selector.php
@@ -0,0 +1,20 @@
+<?php
+   use yii\helpers\Html;
+   use yii\helpers\HtmlPurifier;
+?>
+
+<!--
+TODO:
+set an isActive state for the selected resource
+output all of the resource types
+-->
+
+<div class="section-selector">
+  <div class="d-flex justify-content-center pt-30 pb-30 text-uppercase">
+    <a class="module-navigation-element active" href=""><?= $model->display_name ?></a>
+    <a class="module-navigation-element" href=""></a>
+    <!-- <a class="module-navigation-element" href=""></a>
+    <a class="module-navigation-element" href=""></a>
+    <a class="module-navigation-element" href=""></a> -->
+  </div>
+</div>
