cd5118e4882644f46f538c12a5506893db91a232
andy <gfxdsign@gmail.com>
2019-11-13 21:59:33 -0800

    added signin-invoice modal (redundant)

--- views/_modals/_signin-invoice-modal.php
+++ views/_modals/_signin-invoice-modal.php
@@ -1,4 +1,4 @@
-<div class="modal fade" id="signin-modal" tabindex="-1" role="dialog" aria-labelledby="signin-modal" aria-hidden="true">
+<div class="modal fade" id="signin-invoice-modal" tabindex="-1" role="dialog" aria-labelledby="signin-invoice-modal" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content bg-black text-center">
       <div class="modal-header">
@@ -6,8 +6,8 @@
         </button>
       </div>
       <div class="modal-body">
-        <h2 class="mb-30 c-white">Upvote</h2>
-        <h6 class="mb-30 c-gray-1">You must be logged in to upvote.</h6>
+        <h2 class="mb-30 c-white">Invoice</h2>
+        <h6 class="mb-30 c-gray-1">You must be logged in to pay invoice.</h6>
         <a class="btn btn-rect-lg btn-block mb-20 push-button" href="/site/signup">Join</a>
         <a class="link medium close-modal" href="/site/login">Login</a>
       </div>
