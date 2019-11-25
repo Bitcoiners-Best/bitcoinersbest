d2ee644c23b03b1fb96ab39adb313af4730f716b
andy <gfxdsign@gmail.com>
2019-11-25 13:46:07 -0800

    vue-router + axios

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 import Vue from 'vue';
 import App from './App.vue';
+import axios from 'axios';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: [
+    { path: '/', component: HelloWorld }
+  ]
+});
 
 new Vue({
+    router,
     el: '#app',
     render: h => h(App)
 })
