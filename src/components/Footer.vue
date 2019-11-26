ed66ec7d3234df472a21fe432eb6096625409091
andy <gfxdsign@gmail.com>
2019-11-26 11:47:36 -0800

    file loader + footer + privacy

---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer class="footer bg-black">
+      <div class="container-fluid full">
+        <div class="d-flex justify-content-center pb-50 pt-50">
+
+          <h6>
+            <router-link to="/terms" class="link-light medium">Terms</router-link>
+            <router-link to="/privacy" class="link-light medium ml-40">Privacy</router-link>
+          </h6>
+        </div>
+      </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'In The Footer Component'
+    }
+  }
+}
+</script>
+
+<style>
+header {
+
+}
+
+</style>
---
+++ src/views/Terms.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="terms mt-50 fade-in">
+     <div class="section-header pt-20 pb-20 c-white text-center">
+        <h1 class="mb-0">Terms of Service</h1>
+     </div>
+   </section>
+</template>
+
+<script>
+    export default {
+        name: 'Terms',
+    }
+</script>
+
+<style scoped>
+
+</style>
