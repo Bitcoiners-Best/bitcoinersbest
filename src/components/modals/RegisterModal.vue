8c4cfa3ade3be76d5e464f96313d9c805d92d6ed
andy <gfxdsign@gmail.com>
2019-12-04 13:54:23 -0800

    twitter auth url

--- src/components/modals/RegisterModal.vue
+++ src/components/modals/RegisterModal.vue
@@ -2,8 +2,8 @@
   <b-modal hide-footer id="register-modal" centered title="RegisterModal">
     <h2 class="mb-30 c-white">Upvote</h2>
     <h6 class="mb-30 c-gray-1">You must be logged in to upvote.</h6>
-    <a class="btn btn-rect-lg btn-block mb-20 push-button" href="/site/signup">Join</a>
-    <a class="link medium close-modal" href="/site/login">Login</a>
+    <router-link to="/signup" class="btn btn-rect-lg btn-block mb-20 push-button">Join</router-link>
+    <router-link to="/login" class="link medium close-modal">Login</router-link>
     <button @click.prevent="close"
  class="mt-3 border-b border-teal font-semibold">Close</button>
   </b-modal>
