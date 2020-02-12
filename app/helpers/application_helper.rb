b87602676556e2cf2ad333adf8dd447011fc9317
Pablo Fernandez <pfer@me.com>
2020-02-12 15:11:46 +0100

    Set active class on navbar items

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def title(text)
     content_for :title, text
   end
+
+  def navbar_link(path, text)
+    klass = 'nav-item align-self-center'
+    klass << ' active' if request.path == path
+    content_tag :li, class: klass do
+      link_to text, path, class: 'nav-link'
+    end
+  end
 end
