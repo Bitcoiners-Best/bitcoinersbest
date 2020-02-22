f6d31b4f8a4396f03d89504614266b2456213829
Pablo Fernandez <pfer@me.com>
2020-02-22 20:59:36 +0100

    Disable custom images for resources temporarily

--- app/controllers/concerns/metataggable.rb
+++ app/controllers/concerns/metataggable.rb
@@ -14,16 +14,10 @@ module Metataggable
     end
 
     def add_resource_meta_tags
-      puts "runnning add_meta_tags"
       @page_title = @resource.title
 
       description = @resource.description rescue nil
       @page_description = description if description
-
-      if @resource.image && @resource.image.attached?
-        image = rails_representation_url(@resource.image.variant(resize_and_pad: [300, 157]).processed, disposition: "inline")
-        @page_image = image if image
-      end
     end
   end
 end
