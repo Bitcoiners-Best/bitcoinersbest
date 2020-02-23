377d9e8a0f77c83fce5357c36e8565ffb2a07343
Pablo Fernandez <pfer@me.com>
2020-02-23 11:01:45 +0100

    Send notification to background job

---
+++ app/jobs/resource_notifier_job.rb
@@ -0,0 +1,15 @@
+class ResourceNotifierJob < ApplicationJob
+  queue_as :default
+
+  def perform(resource)
+    url = Rails.application.credentials.dig(:webhooks, :keybase, :new_resource)
+
+    if url
+      resource_url = Rails.application.routes.url_helpers.edit_admin_resource_url(resource.id)
+
+      RestClient.get(url, params: {
+        msg: "'#{resource.title}' submitted by #{resource.created_by.name} pending approval: #{resource_url}"
+      })
+    end
+  end
+end
