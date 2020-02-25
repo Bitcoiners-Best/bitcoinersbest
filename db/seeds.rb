cd9f0fb150d89e2ea457e0f479d5094624fcc07c
Pablo Fernandez <pfer@me.com>
2020-02-25 13:17:33 +0100

    Add a seed to make popullating the database easier

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+print "creating users"
+users = 50.times.map do
+  u = User.new(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: 'aaaaaaaaa',
+    admin: false
+  )
+  u.save!
+  u
+end
+puts
+
+creation_times = (1.month.ago.to_i..Time.now.to_i).to_a
+
+print "creating resources"
+resources = 5000.times.map do |i|
+  print " #{i}" if i % 1000 == 0
+  r = Resource.new(
+    created_by: users.sample,
+    resourceable_type: %w(Book Podcast Episode).sample,
+    resourceable_attributes: {
+      title: Faker::Book.title,
+    },
+    created_at: Time.at(creation_times.sample),
+    approved: true
+  )
+  r.save!
+  r
+end
+puts
+
+print "creating votes"
+100000.times do |i|
+  print " #{i}" if i % 1000 == 0
+  Vote.create!(
+    user: users.sample,
+    resource: resources.sample,
+    count: %w(1 10).sample.to_i,
+    settled: true
+  )
+end
+puts
