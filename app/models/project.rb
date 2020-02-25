658969517f30b67d7952ab689b3a53cdf2ce2278
Pablo Fernandez <pfer@me.com>
2020-02-25 22:46:54 +0100

    Adapt project to new model concerns

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,16 @@
 class Project < ApplicationRecord
+  include RemoteImageable
+  include Imageable
+
+  acts_as_taggable_on :authors
+
   has_one :resource, as: :resourceable, dependent: :destroy
   has_one_attached :image
 
   validates :title, presence: true
+  validates :url, url: true
+
+  def item_path
+    Rails.application.routes.url_helpers.project_path(self.resource.slug)
+  end
 end
